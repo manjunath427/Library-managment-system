6574d0960cf5751de0a9498d0c8ab040d9521662
manjunath <manjunath427>
2019-11-19 16:48:19 +0530

    basics of django

---
+++ library/libraryapp/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from libraryapp.models import Book,Author,Category,Member,Record
+from django import forms
+
+class AuthorAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AuthorAdminForm,self).__init__(*args, **kwargs)
+
+    def clean(self):
+        author=self.cleaned_data.get('author_name')
+        if len(author) < 4:
+            raise forms.ValidationError('please enter a valid name,name cannot be less than 4 character',code='invalid')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(AuthorAdminForm,self).save(commit=commit)
+
+
+class BookAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BookAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        price = self.cleaned_data.get('book_price')
+        if len(str(price)) < 500:
+            raise forms.ValidationError('please enter a valid price,price cannot be graterthan than 500 price',
+                                        code='invalid')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(BookAdminForm, self).save(commit=commit)
+
+
+
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    form=BookAdminForm
+    list_display = ('book_name', 'book_price', 'isbn_number', 'author_name', 'category_name',)
+    list_filter = ('category_name',)
+    search_fields = ('book_name',)
+    ordering = ('book_name',)
+
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    form=AuthorAdminForm
+    search_fields = ("author_name",)
+    ordering = ("author_name",)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    list_filter = ('category_name',)
+
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Member)
+admin.site.register(Record)
+
---
+++ library/libraryapp/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    author_name = models.CharField(max_length=100,help_text="enter th author name")
+
+    class Meta:
+        verbose_name_plural="author"
+
+    def __str__(self):
+        return self.author_name
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100,help_text="enter the name of category")
+
+    def __str__(self):
+        return self.category_name
+
+
+class Book(models.Model):
+        book_name = models.CharField(max_length=100, help_text="enter the name of the book")
+        book_price = models.IntegerField(help_text="enter the price of book")
+        isbn_number = models.CharField(max_length=100)
+        author_name=models.ForeignKey(Author, on_delete=models.CASCADE)
+        category_name=models.ForeignKey(Category, on_delete=models.CASCADE)
+
+        def __str__(self):
+            return self.book_name
+
+class Member(models.Model):
+    member_fname = models.CharField(max_length=100,help_text="enter the member fname")
+    member_lname = models.CharField(max_length=100, help_text="enter the member lname")
+    member_phonenumber = models.IntegerField(help_text="enter the phonenumber")
+    member_emailid = models.EmailField(max_length=100, help_text="enter the mail id")
+
+    def __str__(self):
+        return self.member_fname
+
+class Record(models.Model):
+    issue_date = models.DateField()
+    return_date= models.DateField()
+    book_name = models.ForeignKey(Book,on_delete=models.CASCADE)
+    member_fname = models.ForeignKey(Member,on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+        return self.member_fname
+
+
+
+
+
+        # def upper(self):
+        #     return  self.book_name.upper()
+
+
+
+
+
